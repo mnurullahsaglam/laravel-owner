ccba2fcdad819cc8e820382f9f332f5b4f7f0f14
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-06 13:21:17 +0300

    create HasOwner trait

---
+++ src/HasOwner.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Mnurullahsaglam\LaravelOwner;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+use Mnurullahsaglam\LaravelOwner\Exceptions\InvalidSetting;
+
+trait HasOwner
+{
+    protected OwnerSettings $ownerSettings;
+
+    public function getOwnerSettings(): OwnerSettings
+    {
+        return OwnerSettings::create();
+    }
+
+    protected static function bootHasOwner(): void
+    {
+        static::creating(function (Model $model) {
+            $model->addOwnerId();
+        });
+
+        static::updating(function (Model $model) {
+            $model->addOwnerId();
+        });
+    }
+
+    protected function addOwnerId(): void
+    {
+        $this->ownerSettings = $this->getOwnerSettings();
+
+        $this->ensureValidOwnerSettings();
+
+        $this->{$this->ownerSettings->ownerIdColumn} = Auth::id();
+    }
+
+    protected function ensureValidOwnerSettings(): void
+    {
+        if (! Auth::check()) {
+            throw InvalidSetting::unauthorized();
+        }
+
+        if (! class_exists($this->ownerSettings->ownerModel)) {
+            throw InvalidSetting::invalidOwnerModel();
+        }
+
+        if (! in_array($this->ownerSettings->ownerIdColumn, $this->getFillable())) {
+            throw InvalidSetting::invalidOwnerIdColumn();
+        }
+    }
+
+    public function owner(): BelongsTo
+    {
+        $this->ownerSettings = $this->getOwnerSettings();
+
+        return $this->belongsTo($this->ownerSettings->ownerModel, $this->ownerSettings->ownerIdColumn);
+    }
+}
--- src/Traits/HasOwner.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Mnurullahsaglam\LaravelOwner\Traits;
-
-class HasOwner
-{
-
-}
