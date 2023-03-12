8fb9fa0791c6dbfb86828c83a47f1adf3fc07b7e
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-13 00:47:32 +0300

    add condition before each HasOwnerTest in Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Mnurullahsaglam\LaravelOwner\Tests\Models\TestUserModel;
 use Mnurullahsaglam\LaravelOwner\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+
+uses()->beforeEach(fn () => $this->actingAs(TestUserModel::first()))
+    ->in('HasOwnerTest.php');
