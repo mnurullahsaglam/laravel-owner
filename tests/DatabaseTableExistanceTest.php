c1ce1d4336e4614dbce34ac11d47c571aec5c0bf
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-13 00:53:31 +0300

    update description of test methods in DatabaseTableExistanceTest

--- tests/DatabaseTableExistanceTest.php
+++ tests/DatabaseTableExistanceTest.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Schema;
 
-it('checks if the test users table exists', function () {
+test('test_users table exists', function () {
     expect(Schema::hasTable('test_users'))->toBeTrue();
 });
 
-it('checks if the test posts table exists', function () {
+test('test_posts table exists', function () {
     expect(Schema::hasTable('test_posts'))->toBeTrue();
 });
