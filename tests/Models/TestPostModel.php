287cf7b2dc2d6ee57cedd4775bb3af80ec7709b1
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-13 00:51:39 +0300

    create TestPostModel

---
+++ tests/Models/TestPostModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mnurullahsaglam\LaravelOwner\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Mnurullahsaglam\LaravelOwner\HasOwner;
+use Mnurullahsaglam\LaravelOwner\OwnerSettings;
+
+class TestPostModel extends Model
+{
+    use HasOwner;
+
+    protected $table = 'test_posts';
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+    public function getOwnerSettings(): OwnerSettings
+    {
+        return OwnerSettings::create()
+            ->setOwnerModel(TestUserModel::class)
+            ->setOwnerColumn('user_id');
+    }
+}
