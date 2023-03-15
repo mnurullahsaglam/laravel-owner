cc5524a825103bdc05d1dc13a629dda2ef27bb79
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-15 08:28:11 +0300

    update AssetExistanceTest according to new classes

--- tests/AssetExistanceTest.php
+++ tests/AssetExistanceTest.php
@@ -1,6 +1,8 @@
 <?php
 
-use Mnurullahsaglam\LaravelOwner\Exceptions\InvalidSetting;
+use Mnurullahsaglam\LaravelOwner\Exceptions\InvalidOwnerIdException;
+use Mnurullahsaglam\LaravelOwner\Exceptions\InvalidOwnerModelException;
+use Mnurullahsaglam\LaravelOwner\Exceptions\UnauthorizedException;
 use Mnurullahsaglam\LaravelOwner\HasOwner;
 use Mnurullahsaglam\LaravelOwner\OwnerSettings;
 
@@ -8,10 +10,18 @@ test('OwnerSettings class exists', function () {
     expect(class_exists(OwnerSettings::class))->toBeTrue();
 });
 
-test('InvalidSettings exception class exists', function () {
-    expect(class_exists(InvalidSetting::class))->toBeTrue();
-});
-
 test('HasOwner trait exists', function () {
     expect(trait_exists(HasOwner::class))->toBeTrue();
+});
+
+test('InvalidOwnerIdException class exists', function () {
+    expect(class_exists(InvalidOwnerIdException::class))->toBeTrue();
+});
+
+test('InvalidOwnerModelException class exists', function () {
+    expect(class_exists(InvalidOwnerModelException::class))->toBeTrue();
+});
+
+test('UnauthorizedException class exists', function () {
+    expect(class_exists(UnauthorizedException::class))->toBeTrue();
 });
