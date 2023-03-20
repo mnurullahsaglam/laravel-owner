69f506ce9c7941c684417b5a89470556a9e70f53
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-21 00:47:54 +0300

    add ownerModel and remove previous parameters InvalidOwnerModelException

--- src/Exceptions/InvalidOwnerModelException.php
+++ src/Exceptions/InvalidOwnerModelException.php
@@ -4,15 +4,14 @@ namespace Mnurullahsaglam\LaravelOwner\Exceptions;
 
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 class InvalidOwnerModelException extends Exception
 {
     public function __construct(
-        $message = "Invalid owner model. Please check your owner model.",
-        $code = Response::HTTP_BAD_REQUEST,
-        Throwable $previous = null
+        string $ownerModel,
+        $message = "Invalid owner model: %s. Please check your owner model.",
+        $code = Response::HTTP_BAD_REQUEST
     ) {
-        parent::__construct($message, $code, $previous);
+        parent::__construct(sprintf($message, $ownerModel), $code);
     }
 }
