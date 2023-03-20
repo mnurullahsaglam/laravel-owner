c71eed40b729ed3cfcdfb6412216118874722881
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-21 00:45:16 +0300

    rename ownerId to ownerColumn

---
+++ src/Exceptions/InvalidOwnerColumnException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mnurullahsaglam\LaravelOwner\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class InvalidOwnerColumnException extends Exception
+{
+    public function __construct(
+        string $ownerColumn,
+        $message = "Invalid owner column: %s. Please check your owner column.",
+        $code = Response::HTTP_BAD_REQUEST
+    ) {
+        parent::__construct(sprintf($message, $ownerColumn), $code);
+    }
+}
